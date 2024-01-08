2c8e1379bad73ea4f41cc4f608c175b59cc88939
RafaelC <rafael_caetano_silva@hotmail.com>
2024-01-08 12:18:22 -0300

    Create automates tests dor addToCart

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,9 @@
-export let cart = JSON.parse(localStorage.getItem('cart'));
+export let cart;
+
+loadFromStorage();
+
+export function loadFromStorage() {
+  cart = JSON.parse(localStorage.getItem('cart'));
 
 if (!cart) {
   cart = [{
@@ -11,7 +16,7 @@ if (!cart) {
     deliveryOptionId: '2'
   }];
 }
-
+}
 
 function saveToStorage() {
   localStorage.setItem('cart', JSON.stringify(cart));
@@ -26,10 +31,10 @@ export function addToCart (productId){
       }
     });
   
-    const quantitySelector = document.querySelector(
+     const quantitySelector = document.querySelector(
         `.js-quantity-selector-${productId}`);
   
-    const quantity = Number(quantitySelector.value);
+     const quantity = Number(quantitySelector.value);
   
   
   
